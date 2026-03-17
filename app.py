7c1e9d0c14af66cb2c3a9f3a6cc36a8952a6ce54
KarlMarx29 <kmarx8070@zohomail.in>
2026-03-17 18:35:09 +0530

    v2

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.express as px
 from db import get_db_connection
 import json
 import osmnx as ox
+import os
 
 app = Flask(__name__)
 app.secret_key="madurai"
@@ -573,5 +574,7 @@ def logout():
 
     return redirect('/')
 
-if __name__=='__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
